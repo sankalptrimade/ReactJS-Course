e492ce365a3aad56662903695b70103c2838fa7d
sankalptrimade <sankalptrimade0014@gmail.com>
2025-03-01 11:36:28 +0530

    Basic structure of react

--- Lecture 1/script.js
+++ Lecture 1/script.js
@@ -78,9 +78,12 @@ Inside we have two sibilings child1 and child2
 again we will pass it as an array 
 */
 
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "I am an h1 tag"),
+      React.createElement("h1", {}, "Hi sankalp"),
       React.createElement("h2", {}, "I am an h2 tag"),
     ]),
     React.createElement("div", { id: "child2" }, [
